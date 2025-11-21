f069d33b3c7899a8d8c3981f2aa67af80638ab64
Ashwini-418 <Ashwini-418@users.noreply.github.com>
2025-11-21 22:22:03 +0530

    Add complete Angular frontend with login, dashboard, role-based UI, auth guard and interceptor

---
+++ src/app/services/book.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookService {
+  private apiUrl = 'http://localhost:5000/api/books';
+
+  constructor(private http: HttpClient) {}
+
+  getAllBooks(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
+  }
+
+  addBook(title: string, author: string): Observable<any> {
+    return this.http.post(this.apiUrl, { title, author });
+  }
+
+  deleteBook(id: number): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/${id}`);
+  }
+
+  issueBook(id: number): Observable<any> {
+    return this.http.post(`${this.apiUrl}/issue/${id}`, {});
+  }
+
+  getMyBooks(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/my`);
+  }
+}
